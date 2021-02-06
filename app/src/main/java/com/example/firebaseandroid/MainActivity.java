57c6371e87009dc7629fbbfa8f130d0e1a70c798
root <soumojyotiganguly270@gmail.com>
2021-02-07 00:26:11 +0530

    read data from firebase

--- app/src/main/java/com/example/firebaseandroid/MainActivity.java
+++ app/src/main/java/com/example/firebaseandroid/MainActivity.java
@@ -5,19 +5,24 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 
 import android.os.Bundle;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
 
+import java.lang.reflect.Type;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
-
-    private FirebaseFirestore firebaseFirestore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,11 +30,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
 
-        firebaseFirestore = FirebaseFirestore.getInstance();
+        TextView textView = findViewById(R.id.text_view);
+
+        FirebaseFirestore firebaseFirestore = FirebaseFirestore.getInstance();
 
         Map<String, String> userMap = new HashMap<>();
-        userMap.put("name", "Sayan");
-        userMap.put("Roll", "35");
+        userMap.put("name", "Soumo");
+        userMap.put("Roll", "26");
+        
 
         firebaseFirestore.collection("users").add(userMap).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
             @Override
@@ -43,5 +51,39 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        CollectionReference collectionReference = firebaseFirestore.collection("users");
+        collectionReference.get().addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+            @Override
+            public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                try {
+                    List<Stu> types = queryDocumentSnapshots.toObjects(Stu.class);
+                    for (Stu i : types) {
+                        textView.append(i.getName() + " " + i.getRoll() + "\n\n\n");
+                    }
+
+                } catch (Exception e) {
+                    Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+
+    }
+}
+
+class Stu {
+    String name;
+    String Roll;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRoll() {
+        return Roll;
     }
 }
